c7e4e4bc1293b7db3863dee16999d9f24d44f078
drewc <drewc@tech.coop>
2007-09-06 17:23:16 -0700

    more cruft!
    darcs-hash:20070907002316-39164-2e2b286db9de25227c6f9239dd145722fa8d7410.gz

--- src/packages.lisp
+++ src/packages.lisp
@@ -1,33 +1,9 @@
-(defpackage :lisp-on-lines
-  (:use :arnesi
-	;:iterate
-	;:meta-model
-	:common-lisp
-	;:it.bese.ucw
-	;:clsql
-	:contextl)
-  (:nicknames :lol :mewa)
+(defpackage #:lisp-on-lines
+  (:use 
+	#:common-lisp
+	#:contextl)
+  (:nicknames #:lol)
 
-  #+nil(:shadowing-import-from
-   :ucw
-   :parent)
-  
-  #+nil(:shadowing-import-from
-   :iterate
-   :with)
-
-#+nil  (:shadowing-import-from
-   :clsql
-   :time-difference
-   :make-time
-   :time-ymd
-   :date-ymd
-   :date
-   :get-time
-   :time-element
-   :time+
-   :date-element)
-  
   (:export
 
    #:find-description
@@ -42,65 +18,5 @@
    #:find-attribute
    #:attribute-label
 
-
-
-   ;;;; CLSQL meta-model/default attributes definers
-   ;;;; TODO: should be moved to meta-model,
-   ;;;; with lol specific function implemented like the
-   ;;;; CLOS meta-model
-   :define-view-for-table
-   :define-views-for-database
-
-
-   ;;;; The LoL 3 displays
-   #:defdisplay
-   #:defdescription
-   #:defattribute
-   #:display
-   #:display*
-   #:display-using-description
-   #:display-attribute
-   #:call-display
-
-   #:standard-attribute
-   #:attribute.name
-
-;;;; Standard Layers
-
-   #:editor
-   #:one-line
-   #:as-string
-   #:as-table
-;;;; Wrapping layers
-   #:wrap-div
-   #:wrap-link
-   
-   #:show-attribute-labels
-   ;;;; "Lines", the newest creation.
-   :defline
-
-   ;;;; A macro shortcut for creating ucw actions
-   :action
-
-   ;;;; Mewa Exports
-   :find-occurence
-
-   ;;attributes
-   :attributes
-   :attribute-value
-   :define-attributes
-   :with-default-attributes
-   :set-default-attributes
-   :set-attribute
-   :find-attribute
-
-   ;; Some CRUD
-   #:crud
-   #:crud-editor
-   #:crud-viewer
-   #:crud-summary
-   #:crud-database
-   #:instance))
-
 (cl:defpackage #:lol-test
-  (:use :cl :lisp-on-lines :stefil :contextl))
+  (:use #:cl #:lisp-on-lines #:stefil #:contextl))
